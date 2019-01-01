a66b7c7596b48cc70a7842f12fe08c4d4cc8b5ec
Jo Colina <jo.colina@weezevent.com>
2019-01-01 08:40:06 +0100

    Update to 0.0.5 + check package version with -v

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 const redfox = require('./utils/redfox');
-
 const server = require('./app');
+
+const pkg = require('./package.json');
+
+if (process.argv && process.argv.indexOf('-v') > -1) {
+	return redfox.success(`monitor version ${pkg.version}`);
+}
 
 const port = process.env.PORT || 1234;
 server.listen(port, (err) => {
